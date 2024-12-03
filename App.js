5c2c93249b31ad13ecf53c6127c384d0d6b0054c
AndresMorquecho <morquechoisrael@hotmail.com>
2024-12-02 23:49:29 -0500

    Cambio de nombre componente ContactsScreen

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 
       <Stack.Navigator>
         <Stack.Screen name='WellcomeAndres' component={Home} />
-        <Stack.Screen name='ContactsNav' component={Contacts} />
+        <Stack.Screen name='CallNumber' component={Contacts} />
         <Stack.Screen name='ProductNavigation' component={Product}/>
 
 
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -11,7 +11,7 @@ export const Home = ({ navigation }) => {
                 <Button
                     title='CONTACTS'
                     onPress={() => {
-                        navigation.navigate('ContactsNav');
+                        navigation.navigate('CallNumber');
                     }}
                 />
             </View>
